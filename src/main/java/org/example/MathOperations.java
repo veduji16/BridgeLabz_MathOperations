ea48da2dc0c9cd596240af1b149e4b5742ddf033
veduji16 <jainved01@gmail.com>
2024-03-29 12:11:30 +0530

    Ability to Iterate through List of number and print only if it is even

--- src/main/java/org/example/MathOperations.java
+++ src/main/java/org/example/MathOperations.java
@@ -4,29 +4,23 @@ interface Consumer<T> {
     void accept(T t);
 }
 
+@FunctionalInterface
+interface Predicate<T> {
+    boolean test(T t);
+}
+
 public class MathOperations {
 
     public static void main(String[] args) {
-        Number[] playlist = { new Number(1.2), new Number(3.5), new Number(5.0) };
+        Number[] playlist = { new Number(1.2), new Number(3.5), new Number(5.0), new Number(2.0) };
 
-        System.out.println("Using Class object:");
-        for (Number number : playlist) {
-            number.print();
-        }
+        Predicate<Number> isEven = number -> number.value % 2 == 0;
 
-        System.out.println("\nUsing Anonymous Class:");
+        System.out.println("Even Numbers:");
         for (Number number : playlist) {
-            number.forEach(new Consumer<Number>() {
-                @Override
-                public void accept(Number number) {
-                    System.out.println(number.value);
-                }
-            });
-        }
-
-        System.out.println("\nUsing Lambda Expression:");
-        for (Number number : playlist) {
-            number.forEach(num -> System.out.println(num.value));
+            if (isEven.test(number)) {
+                number.print();
+            }
         }
     }
 }
