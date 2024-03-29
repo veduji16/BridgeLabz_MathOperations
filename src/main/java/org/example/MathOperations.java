1d7938bd25ee24b9125bd3a1152b3f458d656ed1
veduji16 <jainved01@gmail.com>
2024-03-29 11:23:39 +0530

    Ability to show the results of Math Operation of Addition, Subtraction and Division

--- src/main/java/org/example/MathOperations.java
+++ src/main/java/org/example/MathOperations.java
@@ -1,6 +1,7 @@
 package org.example;
 
 public class MathOperations {
+
     public static double performOperation(double a, double b, Operation operation) {
         return operation.calculate(a, b);
     }
@@ -10,18 +11,17 @@ public class MathOperations {
         double calculate(double a, double b);
     }
 
+    public static void showResult(String operationName, double result) {
+        System.out.println(operationName + ": " + result);
+    }
+
     public static void main(String[] args) {
         Operation add = (x, y) -> x + y;
         Operation subtract = (x, y) -> x - y;
-        Operation divide = (x, y) -> x / y;
+        Operation divide = (x, y) -> y != 0 ? x / y : Double.NaN;
 
-        double result = performOperation(5, 3, add);
-        System.out.println("Addition: " + result);
-
-        result = performOperation(10, 4, subtract);
-        System.out.println("Subtraction: " + result);
-
-        result = performOperation(12, 3, divide);
-        System.out.println("Division: " + result);
+        showResult("Addition", performOperation(5, 3, add));
+        showResult("Subtraction", performOperation(10, 4, subtract));
+        showResult("Division", performOperation(12, 3, divide));
     }
 }
